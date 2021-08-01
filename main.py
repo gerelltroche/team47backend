ddd3b8354cc3e35d2b5b9fe0a1b980a7de41e14a
tuliotroche <tgtroche@gmail.com>
2021-08-01 18:20:41 -0400

    committing for meeting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -27,9 +28,9 @@ mean_std_dict = {
  'key': {'mean': 5.301559838160136, 'std': 3.601365222814598},
  'loudness': {'mean': -8.413418041950585, 'std': 4.773060044962174},
  'speechiness': {'mean': 0.11820605036201066, 'std': 0.12779958795305896},
- 'acousticness': {'mean': 0.294434796439521, 'std': 0.3048065194948211},
+ 'acousticsness': {'mean': 0.294434796439521, 'std': 0.3048065194948211},
  'instrumentalness': {'mean': 0.15021815809279174, 'std': 0.3067623266279394},
- 'liveness': {'mean': 0.17988611850510877, 'std': 0.1454453882431879},
+ 'liveliness': {'mean': 0.17988611850510877, 'std': 0.1454453882431879},
  'valence': {'mean': 0.45340037354663404, 'std': 0.24082727978555382},
  'tempo': {'mean': 121.6263309199322, 'std': 29.16301178801898},
  'duration_ms': {'mean': 207722.33698892675, 'std': 76552.8000824765},
@@ -45,23 +46,24 @@ loaded_api = SpotifyAPI('831cc784a86e40f7a94913a7760911c1', '9ec69ad406ef4de69d0
 
 def validate_input(song_features):
     # this should return ALL the missing/invalid features and not just return early.
-    for feature in required_features:
-        if feature not in song_features:
-            return f'{feature} requires a value'
-
-        # have to add validation to makes sure each value is correct.
-        if 0 > song_features[feature] or song_features[feature] > 100:
-            return f'{feature} value should be between 0 - 100'
+    # for feature in required_features:
+    #     if feature not in song_features:
+    #         return f'{feature} requires a value'
+    #
+    #     # have to add validation to makes sure each value is correct.
+    #     if 0 > song_features[feature] or song_features[feature] > 100:
+    #         return f'{feature} value should be between 0 - 100'
+    return False
 
 
 def format_input(song_features):
 
     for key in song_features.keys():
-        song_features[key] = (song_features[key] - mean_std_dict[key.lower()]['mean'])/mean_std_dict[key.lower()]['std']
-    
-    scaled_song = StandardScaler().fit_transform(song_features)
+        song_features[key] = (float(song_features[key]) - mean_std_dict[key.lower()]['mean'])/mean_std_dict[key.lower()]['std']
 
-    return scaled_song
+    df_song_features = pd.DataFrame({k: [v] for k, v in  song_features.items()})
+
+    return df_song_features
 
 
 # Start the server listening.. PORT 5000 by default.
@@ -84,9 +86,11 @@ def predict():
     if error:
         return error
 
-    result = loaded_model.predict_proba(format_input(song_features))
+    input = format_input(song_features)
 
-    return song_features
+    result = loaded_model.predict_proba(input)
+    result = str(result[0][1])
+    return result
 
 
 @app.route("/autocomplete/<id>", methods=['GET'])
